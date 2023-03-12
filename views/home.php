6e024eefeb6a36a7127850abbefc911cd8894ede
Michael K. Ocansey <kabutey@gmail.com>
2023-03-12 04:01:22 +0000

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,7 +1,3 @@
-<?php
-    use App\Core\App;
-    use App\Core\Doc;
-?>
 <!doctype html>
 <html>
     <head>
@@ -12,24 +8,24 @@
         <link rel="shortcut icon" href="/assets/images/quickiedox-icon.png" type="image/x-icon" class="fav-icon" />
         <link href="/assets/css/quickiedox.css" rel="stylesheet">
     </head>
-<body>
-    <div class="h-screen flex bg-slate-500">
+    <body>
+        <div class="h-screen flex bg-slate-500">
 
-        <div class="container mx-auto w-full lg:w-1/2 md:w-3/4 my-auto">
-            <img src="/assets/images/quickiedox-white-logo.svg" alt="QuickieDox Logo" class="h-16 mx-auto" />
-            <img src="/assets/images/create-docs.svg" alt="Create Docs" class="mx-auto w-3/4 mt-12" />
+            <div class="container mx-auto w-full lg:w-1/2 md:w-3/4 my-auto">
+                <img src="/assets/images/quickiedox-white-logo.svg" alt="QuickieDox Logo" class="h-16 mx-auto" />
+                <img src="/assets/images/create-docs.svg" alt="Create Docs" class="mx-auto w-3/4 mt-12" />
 
-            <h1 class="text-5xl md:text-6xl font-semibold mt-16 text-center text-white/50 drop-shadow-md">
-                Quickly create elegant documentation from markdown files
-            </h1>
-            <div class="text-center pt-12 pb-4">
-                <a href="<?php echo get_url_prefix().Doc::stripMdExtension(App::get('default_doc_page')) ?>"
-                   class="hover:bg-black text-white py-4 px-10 rounded-full inline-block text-xl tracking-wider cursor-pointer bg-slate-800">
-                  Read Docs
-                </a>
+                <h1 class="text-5xl md:text-6xl font-semibold mt-16 text-center text-white/50 drop-shadow-md">
+                    Quickly create elegant documentation from markdown files
+                </h1>
+                <div class="text-center pt-12 pb-4">
+                    <a href="<?php echo get_url_prefix().\App\Core\Doc::stripMdExtension(\App\Core\App::get('default_doc_page')) ?>"
+                       class="hover:bg-black text-white py-4 px-10 rounded-full inline-block text-xl tracking-wider cursor-pointer bg-slate-800">
+                      Read Docs
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-    <span class="hidden border border-red-400 text-center"></span>
-</body>
+        <span class="hidden border border-red-400 text-center"></span>
+    </body>
 </html>
