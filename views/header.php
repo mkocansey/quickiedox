77349af4de2e91b80f63a5e394b8d5687c98ab3b
Michael K. Ocansey <kabutey@gmail.com>
2023-03-16 12:48:33 +0000

    Update header.php

--- views/header.php
+++ views/header.php
@@ -11,4 +11,5 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
     <link href="/assets/css/quickiedox.css?<?php echo uniqid(); ?>" rel="stylesheet">
+    <script src="/assets/js/quickiedox.js"></script>
 </head>
